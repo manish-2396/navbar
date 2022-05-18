7b19a7087a72626c67854ea74ff2864a08945b37
manishchouhan2396 <manishchouhan2396@gmail.com>
2022-05-18 09:11:29 +0530

    react app

---
+++ src/conponents/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () =>{
+    return <div>
+            <div class="h1">LOGOBAKERY</div>
+            <div class="h2">
+                <div>Services</div>
+                <div>Projects</div>
+                <div>About</div>
+            </div>
+            <div class="h3">
+                <button>Contact</button>
+            </div>
+        </div>
+    ;
+}
+
+export {Navbar};
